c29d84ac2154011ffdd821c43d7fd190dbddc4b5
zaki82000 <yabdelshafy0@gmail.com>
2023-11-02 20:59:07 +0200

    add 3-infinity_add.py python file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
     total = 0
-    for i in range(len(argv) - 1):
-        total += int(argv[i + 1])
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
