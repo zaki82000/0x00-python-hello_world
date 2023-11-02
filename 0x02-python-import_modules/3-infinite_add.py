2a66ea1939afdbaa0f7babfc0da1978ce13af5e0
zaki82000 <yabdelshafy0@gmail.com>
2023-11-02 21:03:46 +0200

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def argv_sum(argv):
-    sum = 0
-    for index in range(1, len(argv)):
-        sum += int(argv[index])
-    print(sum)
-
-
 if __name__ == "__main__":
-    argv_sum(argv)
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def print_names(module):
-    for name in dir(module):
-        if name[:2] != "__":
-            print("{:s}".format(name))
-
-
 if __name__ == "__main__":
-    print_names(hidden_4)
+
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
