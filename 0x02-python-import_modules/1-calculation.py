35c08d720e7eb65351bc6d67bb23738a6e6f9ac2
zaki82000 <yabdelshafy0@gmail.com>
2023-11-02 19:57:09 +0200

    add 1-calculation.py python file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from calculation_1 import add, sub, mul, div
+
 a = 10
 b = 5
+
 if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
