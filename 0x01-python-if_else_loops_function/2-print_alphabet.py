ec180fe1634eeb83ec73b2d5b04d4b0effb89106
zaki82000 <yabdelshafy0@gmail.com>
2023-11-01 18:35:27 -0700

    rm

--- 0x01-python-if_else_loops_function/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(x), end='')
--- 0x01-python-if_else_loops_function/8-uppercase.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for x in str:
-        if 'a' <= x <= 'z':
-            x = chr(ord(x) - 32)
-        print("{}".format(x), end="")
-    print()
