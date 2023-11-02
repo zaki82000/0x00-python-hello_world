7a011469ac47110b5e67e7ad5ca25a5a72286bd5
zaki82000 <yabdelshafy0@gmail.com>
2023-11-02 21:01:07 +0200

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+import hidden_4
 
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
+def print_names(module):
+    for name in dir(module):
         if name[:2] != "__":
-            print(name)
+            print("{:s}".format(name))
+
+
+if __name__ == "__main__":
+    print_names(hidden_4)
