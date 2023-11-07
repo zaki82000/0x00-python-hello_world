d435efe6362a8cd7fcfd7b5f099dfde188d9ece6
zaki82000 <yabdelshafy0@gmail.com>
2023-11-08 00:54:03 +0200

    update 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ()
-    tuple1 = tuple_a + (0, 0)
-    tuple2 = tuple_b + (0, 0)
-    new_tuple = tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
-    return new_tuple
+def multiple_returns(sentence):
+    tuple = ()
+    if len(sentence) == 0:
+        tuple = 0, "None"
+    else:
+        tuple = len(sentence), sentence[0]
+    return tuple
