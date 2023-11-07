a95c32f31befebc15dd7dd47fc134e65ba881b52
zaki82000 <yabdelshafy0@gmail.com>
2023-11-08 00:51:27 +0200

    update 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
