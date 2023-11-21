9c39c0650bd34965e399654bfb99aeb0aaf9bb91
zaki82000 <yabdelshafy0@gmail.com>
2023-11-22 00:12:17 +0200

    add 1-square file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,13 +3,8 @@
 
 
 class Square:
-    """ that defines a square. """
+    
     def __init__(self, size):
-        """
-        Initialize a Square object with a given size.
-
-        Args:
-            size (int): The size of the square.
-
-        """
-        self._size = size
+        
+        
+        self._Square__size = size
