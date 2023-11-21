f4c3e90362d05a6193c691b9ca79f0e3b6f640fc
zaki82000 <yabdelshafy0@gmail.com>
2023-11-22 00:14:50 +0200

    add 1-square file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,25 @@
 
 
 class Square:
+    """This is a docstring for the Square.
+        class Square that defines a square by: (based on 0-square.py)
+
+        Attributes:
+            attribute1 (int): The value for attribute1.
+
+        Methods:
+            method1(): Private instance attribute: size
+    """
     def __init__(self, size):
+
+        """Constructor for Square.
+
+        Args:
+            attribute1 (int): The value for attribute1.
+
+        """
         self._Square__size = size
+
+        """Private instance attribute: size
+
+        """
