6c4d247292cd2bccc4614c885ca149268301861b
zaki82000 <yabdelshafy0@gmail.com>
2023-11-27 20:32:24 +0200

    med
    add rectangle file

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-list_division = __import__('4-list_division').list_division
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
 
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, 4]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
-
-print("--")
-
-my_l_1 = [10, 8, 4, 4]
-my_l_2 = [2, 0, "H", 2, 7]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This is an empty class called Square. """
+""" This is an class called Square. """
 
 
 class Square:
