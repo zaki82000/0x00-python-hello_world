22eaf8901a46dc1eb8185a61dcc385714f8de28f
zaki82000 <yabdelshafy0@gmail.com>
2023-11-29 00:36:11 +0200

    add 19-copy_list python file

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def copy_list(l):   # function def copy_list(l): that returns a copy of a list.
-    return l   # function return element
+def copy_list(list):
+    return list   # function that returns a copy of a list.
