27fd113dcf9beaa95f85e50599a2880f86859077
zaki82000 <yabdelshafy0@gmail.com>
2023-12-11 09:06:30 +0200

    add 0-add_integer function file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,14 @@
         Adds two number together.
             Return the sum of this numbers.
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two number together.
     """
-    if isinstance(a, int): #:param a: must be an integer.
-        if isinstance(b, int): #:param b: must be an integer.
+    if isinstance(a, int):  # :param a: must be an integer.
+        if isinstance(b, int):  # :param b: must be an integer.
             return a + b
         elif isinstance(b, float):
             return int(a + b)
