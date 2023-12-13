80a33e4d55f02174ff021cebd32fe5667b904972
zaki82000 <yabdelshafy0@gmail.com>
2023-12-13 09:17:07 +0200

    add 2-matrix_divided function file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,8 @@
 """
 function that divides all elements of a matrix.
 """
+
+
 def matrix_divided(matrix, div):
     """
     function that divides all elements of a matrix.
@@ -21,22 +23,21 @@ def matrix_divided(matrix, div):
         ZeroDivisionError : division by zero.
     """
     result_list = []
-    for l in  matrix:
+    for list in matrix:
         sub_result_list = []
         if not all(len(row) == len(matrix[0]) for row in matrix):
-          raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
         else:
-            for i in l:
+            for i in list:
                 if not isinstance(i, (int, float)):
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                elif not isinstance(div, (int,float)):
+                    raise TypeError("matrix must be a matrix, list of lists) of integers/floats")
+                elif not isinstance(div, (int, float)):
                     raise TypeError("div must be a number")
                 elif div == 0:
                     raise ZeroDivisionError("division by zero")
                 else:
-                    result = round (i / div, 2)
+                    result = round(i / div, 2)
                     sub_result_list.append(result)
             result_list.append(sub_result_list)
 
-    return(result_list)
-        
+    return (result_list)
