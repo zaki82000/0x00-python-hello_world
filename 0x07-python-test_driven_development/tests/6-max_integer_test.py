f9c761fc6583462bfdfc28110b63345838186dfa
zaki82000 <yabdelshafy0@gmail.com>
2023-12-19 09:10:16 +0200

    add max_intger function unittest file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,12 +8,28 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
-        # Use assertEqual to check to the largest number
+        # Use assertEqual to check the largest number
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-    
-    def max_at_the_beginning(self):
-        # Use assertEqualto check to the largest number at beginning
+
+    def test_max_at_beginning(self):
+        # Use assertEqual to check the largest number at beginning
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+    
+    def test_max_in_middle(self):
+        # Use assertEqual to check the largest number in middle
+        self.assertEqual(max_integer([4, 3, 5, 2, 1]), 5)
+
+    def test_one_negative_number(self):
+        # Test one negative number in the list
+        self.assertEqual(max_integer([-1, 1, 2, 3, 4]), 4)
+    
+    def test_only_negative_number(self):
+        # Test only negative number in the list
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_one_negative_number(self):
+        # Test ist of one element
+        self.assertEqual(max_integer([1]), 1)
 
     def test_empty_list(self):
         # Use assertIsNone to check if the list is empty
@@ -21,7 +37,3 @@ class TestMaxInteger(unittest.TestCase):
         # Use assertRaises to check raises (if element is not integer)
         with self.assertRaises(TypeError):
             max_integer([1, 'a', 3, 4])
-
-
-if __name__ == '__main__':
-    unittest.main()
