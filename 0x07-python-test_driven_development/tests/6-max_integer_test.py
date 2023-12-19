3823ed4b6fd8ab3083139911d14689436b550e51
zaki82000 <yabdelshafy0@gmail.com>
2023-12-19 08:17:17 +0200

    add max_intger function unittest file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,13 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
-        # Use assertTrue to check to the largest number
-        self.assertTrue(max_integer([1, 2, 3, 4]), 4)
+        # Use assertEqual to check to the largest number
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
     
+    def max_at_the_beginning(self):
+        # Use assertEqualto check to the largest number at beginning
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
     def test_empty_list(self):
         # Use assertIsNone to check if the list is empty
         self.assertIsNone(max_integer([]), None)
