74e07bce4884034a4affba09a1f46445999ff984
zaki82000 <yabdelshafy0@gmail.com>
2023-12-31 17:13:32 +0200

    add is_same_class function file

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,7 @@
 def inherits_from(obj, a_class):
     """
     function that returns True if the object is an instance of
-    a class that inherited (directly or indirectly) from the specified class
+    a class that inherited (directly or indirectly) from the specified class;
+    otherwise False.
     """
-    return isinstance(obj, a_class)and a_class != bool
+    return isinstance(obj, a_class) and type(obj) != a_class
