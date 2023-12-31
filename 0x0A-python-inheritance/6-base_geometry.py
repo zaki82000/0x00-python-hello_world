e01649c02476608f0b8818dbfcbd2ae1eaf068f3
zaki82000 <yabdelshafy0@gmail.com>
2023-12-31 20:40:06 +0200

    add base_geometry class file

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,7 +2,7 @@
 """not allowed to import any module"""
 
 
-class BaseGeometry:
+class BaseGeometry:  # class BaseGeometry
     def area(self):  # Public instance method:
         """raises an Exception with the message"""
         raise Exception("area() is not implemented")
