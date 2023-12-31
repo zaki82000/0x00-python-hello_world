3f11f49ef4a64dfade54e2cfba19f7089deca8c5
zaki82000 <yabdelshafy0@gmail.com>
2023-12-31 16:41:24 +0200

    add is same class function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,8 +5,4 @@
 def is_same_class(obj, a_class):
     """function that returns True
     if the object is exactly an instance of the specified class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        """otherwise False"""
-        return False
+    return isinstance(obj, a_class) and a_class != object
