ee0dc60f75ef8c2398358b6137d51b4cece3e7ef
zaki82000 <yabdelshafy0@gmail.com>
2023-12-31 21:03:21 +0200

    add read_file function file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""not allowed to import any module"""
+
+
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding="UTF-8") as file:
-        result = file.read()
-        print(result)
+    with open(filename, 'r',) as file:
+        print(file.read(), end="")
