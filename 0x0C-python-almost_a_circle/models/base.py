db8c561520a0631411ac037d9f6fb2f899b0a0f7
zaki82000 <yabdelshafy0@gmail.com>
2024-01-01 22:24:29 +0200

    add file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""not allowed to import any module"""
+
+
+class Base:
+    __nb_objects = 0  # private class attribute
+
+    def __init__(self, id=None):
+        if id is None:
+            self.id = Base.__nb_objects + 1
+            Base.__nb_objects += 1
+        else:
+            """
+            otherwise, increment __nb_objects and
+            assign the new value to the public instance attribute id
+            """
+            self.id = id
