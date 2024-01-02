527fae7606b1a6070d5329ec287e963a5c35a3e5
zaki82000 <yabdelshafy0@gmail.com>
2024-01-02 10:29:45 +0200

    add rectangle class file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-
+""" Importing the Base class"""
 from models.base import Base
 
 
@@ -13,8 +13,8 @@ class Rectangle(Base):
         args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the top-left corner of the rectangle.
-            y (int, optional): The y-coordinate of the top-left corner of the rectangle.
+            x (int, optional): The x-coordinate of corner of the rectangle.
+            y (int, optional): The y-coordinate of corner of the rectangle.
             id (int, optional): The unique identifier.
         """
 
@@ -23,3 +23,35 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
