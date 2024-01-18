0e035b50baf981a4b7ce1a20f73a45355575e7b7
zaki82000 <yabdelshafy0@gmail.com>
2024-01-19 01:38:55 +0200

    add 5-full_table.sql  file

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,21 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+-- Set the database to hbtn_0c_0
+USE hbtn_0c_0;
+
+-- Show the full description of the table first_table
+SELECT 
+    COLUMN_NAME,
+    ORDINAL_POSITION,
+    COLUMN_DEFAULT,
+    IS_NULLABLE,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH,
+    NUMERIC_PRECISION,
+    NUMERIC_SCALE,
+    COLUMN_TYPE,
+    COLUMN_KEY,
+    EXTRA
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_NAME = 'first_table';
