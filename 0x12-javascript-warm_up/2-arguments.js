b5d11754ebee22d4bed018114dba5c5eba2875ee
zaki82000 <yabdelshafy0@gmail.com>
2024-02-13 01:32:23 +0200

    add argument javascript file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 function countArgs () {
-  return process.argv.length -2;
+  return process.argv.length - 2;
 }
 
 const numArg = countArgs();
@@ -9,7 +9,7 @@ const numArg = countArgs();
 if (numArg === 0) {
   console.log('No argument');
 } else if (numArg === 1) {
-  console.log('Argument found"');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
