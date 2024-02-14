13d34c0e01f13721a4aeba41bef0c8bd033ae80e
zaki82000 <yabdelshafy0@gmail.com>
2024-02-14 02:46:45 +0200

    add to integer javascript file

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function toInteger () {
+  const myNumber = parseInt(process.argv[2]);
+  if (!isNaN(myNumber)) {
+    console.log('My number: ' + myNumber);
+  } else {
+    console.log('Not a Number');
+  }
+}
+toInteger();
