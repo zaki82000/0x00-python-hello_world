389a9d3d96b85cd71cda9c1c6b4895c4e69cd2d8
zaki82000 <yabdelshafy0@gmail.com>
2024-02-15 05:03:41 +0200

    add square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function square () {
+  const x = parseInt(process.argv[2]);
+  if (isNaN(x)) {
+    console.log('Missing size');
+  } else {
+    for (let i = 0; i < x; i++) {
+      let row = '';
+      for (let j = 0; j < x; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+square();
