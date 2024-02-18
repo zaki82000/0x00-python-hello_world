82e57bbe1b9e2f31600ff4cdb8944f5df50dd267
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 00:57:24 +0200

    add selcet state file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    
+    db_connect = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+        )
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = db_cursor.fetchall()
+
+    for row in rows :
+        print (row)
+
+        db_cursor.close()
+        db_connect.close()
+
