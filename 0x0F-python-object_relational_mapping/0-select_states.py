685b18060c06bcd892c8cd70c13f3bb684fafd71
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 00:59:21 +0200

    add selcet state file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    
+
     db_connect = MySQLdb.connect(
         host='localhost',
         port=3306,
@@ -24,4 +28,3 @@ if __name__ == "__main__":
 
         db_cursor.close()
         db_connect.close()
-
