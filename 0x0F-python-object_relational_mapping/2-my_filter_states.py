865dacbd1c6270c49256d20174b3cc99b5388531
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 03:19:46 +0200

    update file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 from sys import argv
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
+script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+But this time, write one that is safe from MySQL injections!
 """
 
 from sys import argv
