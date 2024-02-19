7c27f56090012549196331adb2bf0806c7df109b
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 03:16:50 +0200

    add cities by state pyrhon file

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db_connect = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3],
+    )
+
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        join states ON cities.state_id=states.id
+        """
+        )
+
+    rows = db_cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    db_cursor.close()
+    db_connect.close()
