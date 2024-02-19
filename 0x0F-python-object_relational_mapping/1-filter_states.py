5eb4957fe6a02327f928cc9ee154df101c594ebf
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 02:00:34 +0200

    add filter states python file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     rows = db_cursor.fetchall()
 
