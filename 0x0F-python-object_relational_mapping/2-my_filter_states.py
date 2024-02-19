cc2e34e3c0a04082f42b92b67767204fcef97551
zaki82000 <yabdelshafy0@gmail.com>
2024-02-19 02:31:48 +0200

    add filter state python file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db_connect = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3],
+    )
+
+    db_cursor = db_connect.cursor()
+
+    state_name_pattern = argv[4] + '%'
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+        (state_name_pattern,))
+
+    rows = db_cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    db_cursor.close()
+    db_connect.close()
