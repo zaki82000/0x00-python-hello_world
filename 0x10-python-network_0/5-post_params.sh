a835030157c127a7caf499bba951817ae476d5f2
zaki82000 <yabdelshafy0@gmail.com>
2024-03-01 03:04:38 +0200

    add podt params bash file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL, and displays the body of the response
-curl -s -   X POST -H "email: email" -H "subject: I will always be here for PLD"
+curl -s -X POST -d "email=email&subject=I will always be here for PLD" "$1"
