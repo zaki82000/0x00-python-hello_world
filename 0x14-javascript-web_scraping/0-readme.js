953eb575158c283047a15ddddb15c31e44b5f694
zaki82000 <yabdelshafy0@gmail.com>
2024-03-27 15:38:43 +0200

    add readme javascript file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,14 +3,14 @@
 const fs = require('fs');
 
 // Function to read and print file content
-function printFileContent(filePath) {
-    // Read file asynchronously
+function printFileContent (filePath) {
+  // Read file asynchronously
   fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data);
-      }
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
   });
 }
 const filePath = process.argv[2];
